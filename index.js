d01cc5391adca19c03ea63c16b9d6b3efc051484
Xao Yang <xao.yang@gusto.com>
2021-09-28 11:02:47 -0500

    created repo

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post('/leadform', (req, res) => {
+  console.log(JSON.stringify(req.body, null, 2));
+  res.send('Got a POST request');
+});
+
+app.listen(port, () => {
+  console.log(`Simple server listening at http://localhost: ${port}`);
+});
